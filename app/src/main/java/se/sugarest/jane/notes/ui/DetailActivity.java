432381e39bb6e5c5818cb2b7ec734ca3865160a1
jinyanliu <jane.jinyan.liu@gmail.com>
2017-10-18 04:26:14 +0200

    Set up menu items for Detail Activity.

--- app/src/main/java/se/sugarest/jane/notes/ui/DetailActivity.java
+++ app/src/main/java/se/sugarest/jane/notes/ui/DetailActivity.java
@@ -3,6 +3,8 @@ package se.sugarest.jane.notes.ui;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import se.sugarest.jane.notes.R;
 
@@ -17,4 +19,25 @@ public class DetailActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_detail_activity, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_save:
+                createAndPostNote();
+                finish();
+                return true;
+        }
+        return true;
+    }
+
+    private void createAndPostNote() {
+    }
+
 }
