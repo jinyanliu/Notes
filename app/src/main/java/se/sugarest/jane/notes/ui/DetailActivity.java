bd10ce68903735f0ac8037b2a5c89c85112f234a
jinyanliu <jane.jinyan.liu@gmail.com>
2017-10-18 05:10:57 +0200

    Rearrange activity_detail xml file.

--- app/src/main/java/se/sugarest/jane/notes/ui/DetailActivity.java
+++ app/src/main/java/se/sugarest/jane/notes/ui/DetailActivity.java
@@ -6,6 +6,7 @@ import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import se.sugarest.jane.notes.R;
@@ -18,18 +19,27 @@ public class DetailActivity extends AppCompatActivity {
 
     private TextView mIdTextView;
 
+    private EditText mTitleEditText;
+
+    private EditText mDescriptionEditText;
+
+    private int mNoteId;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
 
         mIdTextView = (TextView) findViewById(R.id.tv_note_id);
+        mTitleEditText = (EditText) findViewById(R.id.et_note_title);
+        mDescriptionEditText = (EditText) findViewById(R.id.et_note_description);
 
         Intent intentThatStartedThisActivity = getIntent();
         if (intentThatStartedThisActivity != null) {
             if (intentThatStartedThisActivity.hasExtra("current_note_size")) {
                 String currentNotesSize = getIntent().getExtras().getString("current_note_size");
-                mIdTextView.setText(String.valueOf(Integer.parseInt(currentNotesSize) + 1));
+                mNoteId = Integer.parseInt(currentNotesSize) + 1;
+                mIdTextView.setText(String.valueOf(mNoteId));
             }
         }
     }
@@ -52,6 +62,9 @@ public class DetailActivity extends AppCompatActivity {
     }
 
     private void createAndPostNote() {
+
+        String noteTitleString = mTitleEditText.getText().toString();
+        String noteDescriptionString = mDescriptionEditText.getText().toString();
     }
 
 }
