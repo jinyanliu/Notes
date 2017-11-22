35f49d0464f4af81b136244789f57cbd84b58c6d
jinyanliu <jane.jinyan.liu@gmail.com>
2017-11-22 12:51:13 +0100

    Introduce IdlingResource to control Espresso while the app is doing long-running operations, e.g., fetch notes from Notes API.

--- app/src/androidTest/java/se/sugarest/jane/notes/mainActivityTests/RecyclerViewIntentVerificationMainActivityTest.java
+++ app/src/androidTest/java/se/sugarest/jane/notes/mainActivityTests/RecyclerViewIntentVerificationMainActivityTest.java
@@ -1,10 +1,12 @@
 package se.sugarest.jane.notes.mainActivityTests;
 
 import android.app.Activity;
+import android.support.test.espresso.IdlingResource;
 import android.support.test.espresso.contrib.RecyclerViewActions;
 import android.support.test.espresso.intent.rule.IntentsTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
@@ -16,6 +18,7 @@ import se.sugarest.jane.notes.ui.MainActivity;
 
 import static android.app.Instrumentation.ActivityResult;
 import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.IdlingRegistry.getInstance;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.intent.Intents.intended;
 import static android.support.test.espresso.intent.Intents.intending;
@@ -35,12 +38,21 @@ import static se.sugarest.jane.notes.util.Constant.INTENT_NOTE_POSITION_TITLE;
 @RunWith(AndroidJUnit4.class)
 public class RecyclerViewIntentVerificationMainActivityTest {
 
+    private IdlingResource mIdlingResource;
+
     @Rule
-    public IntentsTestRule<MainActivity> intentsTestRule = new IntentsTestRule<>(MainActivity.class);
+    public IntentsTestRule<MainActivity> mIntentsTestRule = new IntentsTestRule<>(MainActivity.class);
 
     @Before
     public void stubAllExternalIntents() {
         intending(not(isInternal())).respondWith(new ActivityResult(Activity.RESULT_OK, null));
+    }
+
+    // Registers any resource that needs to be synchronized with Espresso before the test is run.
+    @Before
+    public void registerIdlingResource() {
+        mIdlingResource = mIntentsTestRule.getActivity().getIdlingResource();
+        getInstance().register(mIdlingResource);
     }
 
     @Test
@@ -49,5 +61,13 @@ public class RecyclerViewIntentVerificationMainActivityTest {
         intended(allOf(hasExtraWithKey(INTENT_NOTE_ID_TITLE),
                 hasExtra(INTENT_NOTE_POSITION_TITLE, 0),
                 hasComponent(DetailActivity.class.getName())));
+    }
+
+    // Remember to unregister resources when not needed to avoid malfunction.
+    @After
+    public void unregisterIdlingResource() {
+        if (mIdlingResource != null) {
+            getInstance().unregister(mIdlingResource);
+        }
     }
 }
--- app/src/main/java/se/sugarest/jane/notes/ui/MainActivity.java
+++ app/src/main/java/se/sugarest/jane/notes/ui/MainActivity.java
@@ -2,7 +2,11 @@ package se.sugarest.jane.notes.ui;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.annotation.VisibleForTesting;
 import android.support.design.widget.FloatingActionButton;
+import android.support.test.espresso.IdlingResource;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -26,6 +30,7 @@ import se.sugarest.jane.notes.R;
 import se.sugarest.jane.notes.api.NotesClient;
 import se.sugarest.jane.notes.data.NoteAdapter;
 import se.sugarest.jane.notes.data.type.Note;
+import se.sugarest.jane.notes.idlingResource.SimpleIdlingResource;
 
 import static se.sugarest.jane.notes.util.Constant.INTENT_CURRENT_NOTE_SIZE_TITLE;
 import static se.sugarest.jane.notes.util.Constant.INTENT_NOTE_ID_TITLE;
@@ -45,6 +50,22 @@ public class MainActivity extends AppCompatActivity implements NoteAdapter.NoteA
     private int mNotesSize;
     private Toast mToast;
 
+    // The Idling Resource which will be null in production.
+    @Nullable
+    private SimpleIdlingResource mIdlingResource;
+
+    /**
+     * Only called from test, creates and returns a new {@link SimpleIdlingResource}.
+     */
+    @VisibleForTesting
+    @NonNull
+    public IdlingResource getIdlingResource() {
+        if (mIdlingResource == null) {
+            mIdlingResource = new SimpleIdlingResource();
+        }
+        return mIdlingResource;
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -55,6 +76,9 @@ public class MainActivity extends AppCompatActivity implements NoteAdapter.NoteA
         setUpFabButtonOnClick();
         setUpRecyclerViewWithAdapter();
         sendNetworkRequestGet();
+
+        // Get the IdlingResource instance
+        getIdlingResource();
     }
 
     /**
@@ -70,6 +94,11 @@ public class MainActivity extends AppCompatActivity implements NoteAdapter.NoteA
     // Use External Library Retrofit to GET notes list.
     // Reference: https://github.com/square/retrofit
     private void sendNetworkRequestGet() {
+
+        if (mIdlingResource != null) {
+            mIdlingResource.setIdleState(false);
+        }
+
         OkHttpClient.Builder httpClient = new OkHttpClient.Builder();
         Retrofit.Builder builder =
                 new Retrofit.Builder()
@@ -96,6 +125,11 @@ public class MainActivity extends AppCompatActivity implements NoteAdapter.NoteA
                         }
                     });
                     mNoteAdapter.setNotesData(notesList);
+
+                    if (mIdlingResource != null) {
+                        mIdlingResource.setIdleState(true);
+                    }
+
                 } else {
                     showEmptyView();
                 }
